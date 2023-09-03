ea6d400642e8d7603a99bddf0c30704b75c26b4f
Chaman Pratap <chmnprtp@gmail.com>
2023-09-03 17:15:40 +0000

    conversion of data type

---
+++ 01_basics/03-conversionOperation.js
@@ -0,0 +1,24 @@
+let value = "33aa" // null , undefined , "Chaman"
+//console.log(value);
+//console.log(typeof value);
+//console.log(typeof (value));
+let valueinNumber = Number(value)
+//console.log(valueinNumber);
+//console.log(typeof (valueinNumber));
+
+
+//33 => number
+//33aa => NaN
+//null => 0
+// undefined => NaN
+// true => 1 false => 0
+//
+
+
+let isLoggedIn = "";  // ""  , 1 , "chaman"
+let booleanisLoogedIn = Boolean(isLoggedIn);
+console.log(booleanisLoogedIn)
+
+// 1 => true 0 => false
+//"" => false
+//"Chaman" => true
