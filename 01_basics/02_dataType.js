23bf8cd8f35f8f49010378647833a29a5fd3d7de
Chaman Pratap <chmnprtp@gmail.com>
2023-09-03 16:48:08 +0000

    Data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,19 @@
+//number
+//bigInt
+//string    ""
+//boolean  =>  true / false
+//null => standalone value
+// undefined => 
+// symbol 
+
+
+
+//object
+
+
+let age = 5
+console.log(typeof"Chaman")     //string
+console.log(typeof age);        //number
+console.log(typeof null);       //object
+console.log(typeof undefined); // undefined
+
