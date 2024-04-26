326e4793737a86ce5e8ef313ad00a477aad4ad2b
Chaman Pratap <chmnprtp@gmail.com>
2024-04-26 11:30:59 +0000

    conversionOperation

---
+++ 01Basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let age = "26bvh";  // null will result 0 , undefined will result NaN, true=1, false=0, "dfgs"=NaN
+
+// console.log(typeof age);
+// console.log(typeof(age));
+
+const valueInNumber = Number(age);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = 0 // 1=true, ""=false, "cham"=true, null=false, undefined=false
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 22 // 22=22, null=null, undefined=undefined, true=true
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
