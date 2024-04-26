9330c206894bc7fc5a52c7ad1056f727a74281ca
Chaman Pratap <chmnprtp@gmail.com>
2024-04-26 21:26:09 +0000

    Comparisons

---
+++ 01Basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1); //true
+// console.log(2 < 1); //false
+// console.log(2 >= 1);//true
+// console.log(2 <= 1);//false
+// console.log(2 != 1);//true
+// console.log(2 == 1);//false
+
+// console.log("2" > 1); //true
+// console.log(2 > "1"); //true
+
+
+// console.log(null > 0 ); //false  Comparisons convert null to number treat as 0
+// console.log(null == 0); //false  Equality check and comparison works differently
+// console.log(null >= 0); //true
+
+// console.log(undefined == 0); //false
+// console.log(undefined == 1);//false
+
+// ===
+console.log("0" === 0);//false check value and datatype
+console.log("0" == 0); //true
