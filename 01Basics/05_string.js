125eb9062832a8b82d4e63cb817e5f8b3b513998
Chaman Pratap <chmnprtp@gmail.com>
2024-04-27 10:58:56 +0000

    string

---
+++ 01Basics/05_string.js
@@ -0,0 +1,26 @@
+// String interpolation
+
+const name = "hello"
+const age = "24"
+// console.log(name + age);
+
+// console.log(`${name} my name is chaman and my age is ${age}`);
+
+const demo = new String("sjfnds")
+// console.log(demo[0]);
+// console.log(demo.__proto__); // {}
+// console.log(demo.length);
+// console.log(demo.toUpperCase());
+// console.log(demo.charAt(1));
+// console.log(demo.indexOf('s'));
+
+// const str = demo.substring(0,3)// no use of negative
+// console.log(str);
+
+// const str1 = demo.slice(-6,3)
+// console.log(str1);
+
+const newstr = "   chamn  "
+console.log(newstr);
+console.log(newstr.trim());// remove white space
+console.log(newstr.replace("chamn","prtp"));
