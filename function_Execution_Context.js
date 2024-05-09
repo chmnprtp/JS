5c401a8bb96e57142ed2f27c499c2a32bcd82968
Chaman Pratap <chmnprtp@gmail.com>
2024-05-09 19:45:22 +0000

    Execution Context and empty file in js

---
+++ function_Execution_Context.js
@@ -0,0 +1,19 @@
+// https://www.youtube.com/watch?v=gSDncyuGw0s&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP&index=5&ab_channel=AkshaySaini
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a()
+{
+    var x = 10;
+    console.log(x);
+}
+
+
+function b()
+{
+    var x = 100;
+    console.log(x);
+}
