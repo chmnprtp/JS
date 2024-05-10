308facb4691d0a2ec867bfb4ba7330a985c0dcdf
Chaman Pratap <chmnprtp@gmail.com>
2024-05-10 07:44:02 +0000

    closures

---
+++ closures.js
@@ -0,0 +1,29 @@
+function x()
+{
+    var a = 31;
+    functio y()
+    {
+        console.log(a);
+    }
+    return y;
+}
+
+var z = x();
+console.log(z);
+z();
+
+
+
+/* 1.  Function bundled with its lexical environment is known as a closure. 
+2.  Whenever function is returned, even if its vanished in execution context but still it remembers the reference it was pointing to. Its   not just that function alone it returns but the entire closure and that's where it becomes interesting.
+ */
+/* 
+Uses of Closures
+- Model Design Pattern
+-Currying
+-Functions like once
+-Memoize
+-Maintaining state in async world
+-setTimeout
+-Iteration
+-and many more */
