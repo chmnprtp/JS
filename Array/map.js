67d9fe839cb0596e8278de192f622154f2e6e6b4
Chaman Pratap <chmnprtp@gmail.com>
2024-05-20 11:14:23 +0530

    Update map.js

--- Array/map.js
+++ Array/map.js
@@ -1,3 +1,12 @@
+// map() creates a new array from calling a function for every array element.
+
+// map() does not execute the function for empty elements.
+
+// map() does not change the original array.
+
+
+
+
 const arr = [1,2,3,4,5]
 
 let result = arr.map(myfunction);
